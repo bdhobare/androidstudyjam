6c548c5aabab0541518bff03e902c5b4d4f7bead
bdhobare <bdhobare@gmail.com>
2016-10-22 13:12:43 +0300

    Stack builder

--- app/src/main/java/com/miles/sunshine/services/GDGService.java
+++ app/src/main/java/com/miles/sunshine/services/GDGService.java
@@ -2,12 +2,15 @@ package com.miles.sunshine.services;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.v4.app.TaskStackBuilder;
 import android.widget.Toast;
 
+import com.miles.sunshine.MovieActivity;
 import com.miles.sunshine.R;
 
 public class GDGService extends Service {
@@ -21,6 +24,17 @@ public class GDGService extends Service {
                 .setContentTitle("GDG Sessions")
                 .setContentText("Hello world")
                 .setSmallIcon(R.mipmap.ic_launcher);
+
+        Intent resultIntent = new Intent(this, MovieActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MovieActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        builder.setContentIntent(resultPendingIntent);
         NotificationManager manager=
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
