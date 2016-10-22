c422fe7170aa15d185a9607fb26d2afcf6e74af8
bdhobare <bdhobare@gmail.com>
2016-10-22 14:00:26 +0300

    Fragment

---
+++ app/src/main/java/com/miles/sunshine/NewsFragment.java
@@ -0,0 +1,41 @@
+package com.miles.sunshine;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NewsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NewsFragment extends Fragment {
+
+    public NewsFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static NewsFragment newInstance(String param1, String param2) {
+        NewsFragment fragment = new NewsFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.news_stories, container, false);
+
+        return view;
+    }
+
+}
