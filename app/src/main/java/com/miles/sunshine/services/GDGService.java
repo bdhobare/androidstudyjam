4af093dee700d737966b76c9d03b8fdd56489a70
bdhobare <bdhobare@gmail.com>
2016-10-22 13:07:48 +0300

    Notification

--- app/src/main/java/com/miles/sunshine/services/GDGService.java
+++ app/src/main/java/com/miles/sunshine/services/GDGService.java
@@ -1,16 +1,30 @@
 package com.miles.sunshine.services;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.widget.Toast;
+
+import com.miles.sunshine.R;
 
 public class GDGService extends Service {
     public GDGService() {
     }
     @Override
     public int onStartCommand(Intent intent,int flag,int flag0){
-        Toast.makeText(getApplicationContext(),"Service started",Toast.LENGTH_LONG).show();
+        //Toast.makeText(getApplicationContext(),"Service started",Toast.LENGTH_LONG).show();
+        Notification.Builder builder=
+                new Notification.Builder(this)
+                .setContentTitle("GDG Sessions")
+                .setContentText("Hello world")
+                .setSmallIcon(R.mipmap.ic_launcher);
+        NotificationManager manager=
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        manager.notify(5,builder.build());
         return START_STICKY;
     }
     @Override
